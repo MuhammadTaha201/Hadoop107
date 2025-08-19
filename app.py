b267d5badc48f9bb3c2d055500189376510cf299
Muhmmad Taha Ansari <129578946+MuhammadTaha201@users.noreply.github.com>
2025-08-19 12:30:49 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import avg
 
@@ -12,21 +13,21 @@ spark = SparkSession.builder \
 # 2. Read CSV from HDFS
 # -------------------------------
 # Replace "yourname" with your Hadoop username
-file_path = "hdfs:///user/yourname/preprocessed_climate_data.csv"
+file_path = "https://raw.githubusercontent.com/MuhammadTaha201/Hadoop107/refs/heads/main/preprocessed_climate_data.csv"
 
 df = spark.read.csv(
     file_path,
-    header=True,       # first row as header
-    inferSchema=True   # automatically detect column types
+    header=True,       # first row is header
+    inferSchema=True   # detect column types automatically
 )
 
 # -------------------------------
 # 3. Show Data
 # -------------------------------
-print("📌 First 10 rows of dataset:")
+print("First 10 rows of dataset:")
 df.show(10)
 
-print("📌 Schema of dataset:")
+print("Schema of dataset:")
 df.printSchema()
 
 # -------------------------------
@@ -34,12 +35,12 @@ df.printSchema()
 # -------------------------------
 # Average temperature per month
 avg_temp = df.groupBy("Month").agg(avg("Temperature").alias("Avg_Temperature"))
-print("📌 Average Temperature per Month:")
+print("Average Temperature per Month:")
 avg_temp.show()
 
 # Average CO2 levels per location
 avg_co2 = df.groupBy("Location").agg(avg("Co2_levels").alias("Avg_CO2"))
-print("📌 Average CO2 per Location:")
+print("Average CO2 per Location:")
 avg_co2.show()
 
 # -------------------------------
